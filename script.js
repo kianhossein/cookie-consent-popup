d6388a6f68860686f07ebe52106dca1e203120a0
Kian Hossein <kian.j.hossein@gmail.com>
2025-10-01 20:03:28 +0330

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const btn = document.querySelector('#btn');
+// const popup = document.querySelector('.cookie-popup');
+
+// if (localStorage.getItem('cookieAccepted') === 'true') {
+//   popup.style.display = 'none';
+// }
+
+// btn.addEventListener('click', function () {
+//   localStorage.setItem('cookieAccepted', 'true');
+//   popup.style.display = 'none';
+// });
